ff1e6860a45fe4171d18c3899b3c462373b7caed
atgm1113 <atgm1113@gmail.com>
2020-05-11 19:44:27 +0300

    Add setup.py, Update gitignore

--- keras_generic_seq2seq/setup.py
+++ keras_generic_seq2seq/setup.py
@@ -1,39 +1,24 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
     long_description = fh.read()
 
 setuptools.setup(
-
-     name='atgm1113_generic_keras_seq2seq',  
-
-     version='0.1',
-
-     scripts=['keras_generic_seq2seq'] ,
-
-     author="atgm1113",
-
-     author_email="atgm1113@gmail.com",
-
-     description="Simple seq2seq model made with keras",
-
-     long_description=long_description,
-
-   long_description_content_type="text/markdown",
-
-     url="https://github.com/attm/keras-generic-seq2seq",
-
-     packages=setuptools.find_packages(),
-
-     classifiers=[
-
-         "Programming Language :: Python :: 3",
-
-         "License :: OSI Approved :: MIT License",
-
-         "Operating System :: OS Independent",
-
-     ],
-
- )
+    name="atgm1113_keras_generic_seq2seq",
+    version="1.0.0",
+    description="Simple keras seq2seq enc-dec model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/attm/keras-generic-seq2seq",
+    author="atgm1113",
+    author_email="atgm1113@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    py_modules=['keras_generic_seq2seq'],
+    include_package_data=True,
+    install_requires=["tensorflow", "numpy"]
+)
